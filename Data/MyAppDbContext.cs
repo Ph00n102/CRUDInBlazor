50e4f11817bccb756e49a15d1911e0fd0f5cd93a
Ph00n102 <phoonhd10@gmail.com>
2024-09-11 17:24:16 +0700

    first commit

---
+++ Data/MyAppDbContext.cs
@@ -0,0 +1,14 @@
+using CRUDInBlazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDInBlazor.Data
+{
+    public class MyAppDbContext : DbContext
+    {
+        public MyAppDbContext (DbContextOptions<MyAppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set;}
+    }
+}
